548381c0ce199fae7ed8f469ebf6c12a29bf3f01
Willy Weiskopf <william@weiskopf.me>
2014-01-29 01:23:42 -0700

    Resolving media_url problems.

--- scribe/views.py
+++ scribe/views.py
@@ -21,6 +21,8 @@ class EmailDetail(LoginRequiredMixin, EmailMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(EmailDetail, self).get_context_data(**kwargs)
+        from django.templatetags.static import static
+        print(static(context['object'].template.template.url))
         with open(context['object'].template.template.url[1:]) as content_file:
             contents = content_file.read()
             contents = contents.replace("{ CONTENTS HERE }", context['object'].content)
--- thoth/settings.py
+++ thoth/settings.py
@@ -113,6 +113,7 @@ if os.getcwd() == "/app":
     DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
     DEBUG = True
     TEMPLATE_DEBUG = DEBUG
+    MEDIA_URL = 'http://secure-thicket-4638.herokuapp.com/'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
