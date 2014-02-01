051849febb60d00207ad5de28c634e7679b1d00b
Willy Weiskopf <william@weiskopf.me>
2014-02-01 00:56:22 -0700

    Cleanup.

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from authtools.models import AbstractNamedUser, AbstractEmailUser
+from authtools.models import AbstractNamedUser
 
 class User(AbstractNamedUser):
     def username():
