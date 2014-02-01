d92fd773207ec8ddc8be224f7bfeea7b19aaca64
Willy Weiskopf <william@weiskopf.me>
2014-02-01 12:20:21 -0700

    Removed index, twas redundant.

--- thoth/urls.py
+++ thoth/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from thoth.views import Index
+from scribe.views import EmailIndex
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tinymce/', include('tinymce.urls')),
-    url(r'^$', Index.as_view(), name='index'),
+    url(r'^$', EmailIndex.as_view(), name='index'),
     url(r'^accounts/', include('authtools.urls')),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^scribe/', include('scribe.urls', namespace="scribe")),
