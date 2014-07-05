a33c874de5ad58d93f8e4646431b08a57fa89cec
Willy Weiskopf <william@weiskopf.me>
2014-07-05 01:25:36 -0600

    Guess I needed dj-static.

--- thoth/wsgi.py
+++ thoth/wsgi.py
@@ -12,3 +12,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thoth.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+from dj_static import Cling, MediaCling
+application = Cling(get_wsgi_application())
+application = Cling(MediaCling(get_wsgi_application()))
